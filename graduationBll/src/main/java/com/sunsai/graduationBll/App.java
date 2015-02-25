4b788b9b2bb6b7a130ce799904f62f6cf21f1415
sunsai <sunsai.cs@gmail.com>
2015-02-25 19:37:07 +0800

    add Bll and Dal and Common

---
+++ graduationBll/src/main/java/com/sunsai/graduationBll/App.java
@@ -0,0 +1,13 @@
+package com.sunsai.graduationBll;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
